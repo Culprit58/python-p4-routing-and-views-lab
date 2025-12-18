a077e217f677331de034e985e8365c3c4b291dc6
Enock Cheruiyot <cheruiyote518@gmail.com>
2025-12-18 15:28:16 +0300

    lab done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,46 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+
+@app.route('/print/<string:text>')
+def print_text(text):
+    # print to console
+    print(text)
+    # return to browser
+    return text
+
+
+@app.route('/count/<int:num>')
+def count(num):
+    output = ''
+    for i in range(num):
+        output += f"{i}\n"
+    return output
+
+
+@app.route('/math/<int:a>/<op>/<int:b>')
+def math(a, op, b):
+    if op == '+':
+        result = a + b
+    elif op == '-':
+        result = a - b
+    elif op == '*':
+        result = a * b
+    elif op == 'div':
+        result = a / b
+    elif op == '%':
+        result = a % b
+    else:
+        return 'Invalid operation', 400
+
+    return str(result)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
